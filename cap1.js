185c0e15b7a5a75e5660f9f1a938fc8281d48e43
alex <talexroger1997@gmail.com>
2023-12-26 19:15:44 -0500

    Capitulos

--- cap1.js
+++ cap1.js
@@ -90,3 +90,20 @@ document.getElementById('calcularResultado').addEventListener('click', function(
   document.getElementById('resultado').style.display = 'block';
 });
 
+
+window.addEventListener('scroll', function() {
+  var backToTopButton = document.getElementById('back-to-top');
+
+  if (window.pageYOffset > 300) { // Muestra el botón después de desplazarse 300px hacia abajo
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+});
+
+document.getElementById('back-to-top').addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' }); // Hace el scroll suave hacia arriba al hacer clic
+});
+
+
+
